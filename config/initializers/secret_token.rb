3efed976dbba9754a29c77c52db0a4029f036556
Juan Miguel Maniego <jmmaniego@hotmail.com>
2013-12-14 19:32:28 +0800

    JFF App :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstJffApp::Application.config.secret_key_base = '6c4f652f38a627bec0e29fea992a45bd965b9b7a7c8b4f0cabd2b050dd1e4e40ec26d2290ec00e8d1a65d15f893a49e99937229d12f2e2eec6f81b011394a4bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstJffApp::Application.config.session_store :cookie_store, key: '_first_jff_app_session'
