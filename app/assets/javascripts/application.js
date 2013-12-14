6e8bcdf30ef3d8fc022151b9fc1ad558af03da05
Juan Miguel Maniego <jmmaniego@hotmail.com>
2013-12-14 22:07:02 +0800

    LOL

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(e){
+	
+	$('#box').mousedown(function(evt){
+		$(this).mousemove(function(event){
+			$new_element = $("<div class='dot'></div");
+		  $new_element.css("top", event.pageY)
+			$new_element.css("left", event.pageX)
+			
+			$(this).append($new_element)
+		}).mouseup(function(e){
+			$(this).unbind('mousemove');
+	});
+	})
+});
